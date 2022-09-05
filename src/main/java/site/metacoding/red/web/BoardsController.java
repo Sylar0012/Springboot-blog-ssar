b20eeee813c86564a186392bc16733ec7520a12a
Sylar <kmc42692@naver.com>
2022-09-05 09:53:20 +0900

    글쓰기시 로그인 유무 확인

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -1,13 +1,23 @@
 package site.metacoding.red.web;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.users.Users;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardsController {
 
+	private final HttpSession session;
+	
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
 	
@@ -25,6 +35,11 @@ public class BoardsController {
 	
 	@GetMapping("/boards/writeForm")
 	public String writeForm() {
+		Users principal = (Users)session.getAttribute("principal");
+		if(principal == null) {
+			return "redirect:/loginForm";
+		}else {
 		return "boards/writeForm";
+		}
 	}
 }
